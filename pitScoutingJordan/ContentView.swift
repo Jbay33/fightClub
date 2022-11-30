2c97bb7258d14dbdbdf8112d1606266391f24b61
Justin Baylis <jbaylis01@haverfordsd.net>
2022-11-30 13:50:40 -0500

    Initial Commit

---
+++ pitScoutingJordan/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pitScoutingJordan
+//
+//  Created by  on 11/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pitScoutingJordan/pitScoutingJordanApp.swift
@@ -0,0 +1,17 @@
+//
+//  pitScoutingJordanApp.swift
+//  pitScoutingJordan
+//
+//  Created by  on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct pitScoutingJordanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
