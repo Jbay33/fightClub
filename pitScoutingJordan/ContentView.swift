e74421642bf2c6c78a9575101b2045199184c5fd
Justin Baylis <jbaylis01@haverfordsd.net>
2022-12-01 13:59:28 -0500

    I made the list app FIGHT CLUB

--- pitScoutingJordan/ContentView.swift
+++ pitScoutingJordan/ContentView.swift
@@ -6,21 +6,69 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var newTeam = ""
+    @State var teamList = ["484"]
+    @State var showAlert = false
+    @State var deleteAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView(){
+            List{
+                ForEach(teamList,  id:\.self){
+                    teamList in
+                    Text(teamList)
+                }
+                .navigationTitle("Teams")
+            }
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("+"){
+                        showAlert.toggle()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .alert("Add Team", isPresented: $showAlert) {
+                TextField("", text: $newTeam, prompt: Text("Team"))
+                Button("Mibro"){
+                    addTeam()
+                    saveInput()
+                }
+            }
+            .alert("Delete Team", isPresented: $deleteAlert) {
+                Text("Are you sure you want to delete team ")
+                Button("Delete"){
+                    //delete team
+                }
+                Button("Cancel"){
+                    //nothing
+                }
+            }
         }
-        .padding()
+        .onAppear(perform: loadInput)
+    }
+    func addTeam(){
+        withAnimation{
+            teamList.append("\(newTeam)")
+        }
+    }
+    func saveInput(){
+        let defaults = UserDefaults.standard
+        
+        defaults.set(teamList, forKey: "teamList")
+    }
+    func loadInput(){
+        let defaults = UserDefaults.standard
+        
+        if let foundItems = defaults.array(forKey: "teamList") as? [String]{
+            self.teamList = foundItems
+        }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
