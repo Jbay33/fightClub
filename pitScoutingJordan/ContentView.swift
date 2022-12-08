fb349e9548522fe215f8042d5599eb499cb56115
Justin Baylis <jbaylis01@haverfordsd.net>
2022-12-08 13:14:20 -0500

    Cumpit

--- pitScoutingJordan/ContentView.swift
+++ pitScoutingJordan/ContentView.swift
@@ -11,39 +11,45 @@ struct ContentView: View {
     @State var teamList = ["484"]
     @State var showAlert = false
     @State var deleteAlert = false
+    @State var deleteOption = false
+    @State var deleteControl = false
     var body: some View {
         NavigationView(){
-            List{
-                ForEach(teamList,  id:\.self){
-                    teamList in
-                    Text(teamList)
+            VStack{
+                Toggle("Enable Delete", isOn: $deleteControl)
+                List{
+                    ForEach(teamList,  id:\.self){
+                        teamList in
+                        Text(teamList)
+                    }
+                    .onDelete(perform: deleteControl ? delete : nil)
+                    .navigationTitle("Teams")
                 }
-                .navigationTitle("Teams")
-            }
-            .toolbar{
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("+"){
-                        showAlert.toggle()
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("+"){
+                            showAlert.toggle()
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
                     }
                 }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
+                .alert("Add Team", isPresented: $showAlert) {
+                    TextField("", text: $newTeam, prompt: Text("Team"))
+                    Button("Mibro"){
+                        addTeam()
+                        saveInput()
+                    }
                 }
-            }
-            .alert("Add Team", isPresented: $showAlert) {
-                TextField("", text: $newTeam, prompt: Text("Team"))
-                Button("Mibro"){
-                    addTeam()
-                    saveInput()
-                }
-            }
-            .alert("Delete Team", isPresented: $deleteAlert) {
-                Text("Are you sure you want to delete team ")
-                Button("Delete"){
-                    //delete team
-                }
-                Button("Cancel"){
-                    //nothing
+                .alert("Delete Team", isPresented: $deleteAlert) {
+                    Text("Are you sure you want to delete team ")
+                    Button("Delete"){
+                        deleteOption = true
+                    }
+                    Button("Cancel"){
+                        //nothing
+                    }
                 }
             }
         }
@@ -53,6 +59,15 @@ struct ContentView: View {
         withAnimation{
             teamList.append("\(newTeam)")
         }
+    }
+    func deleteFunc(){
+        deleteAlert.toggle()
+    }
+    func delete(at offsets: IndexSet){
+        
+        teamList.remove(atOffsets: offsets)
+        deleteOption = false
+        saveInput()
     }
     func saveInput(){
         let defaults = UserDefaults.standard
